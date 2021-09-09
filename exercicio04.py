3a5bf87e44a4b6e2502c193f984bf9c089c5c75a
EduarTeixeira <88851515+EduarTeixeira@users.noreply.github.com>
2021-09-09 15:49:54 -0300

    Create exercicio04.py

---
+++ exercicio04.py
@@ -0,0 +1,6 @@
+n1 = input("Digite o nome de um carro velho: ")
+n2 = input("Digite o nome outro carro velho: ")
+carros_velhos = [n1, n2]
+todos_os_carros = carros_velhos.copy()
+todos_os_carros.append("Fusca")
+print(todos_os_carros)
