523fe55241c175fb15b4ceb7e1e0eb320426f41e
EduarTeixeira <88851515+EduarTeixeira@users.noreply.github.com>
2021-09-09 15:45:46 -0300

    Create exercicio03.py

---
+++ exercicio03.py
@@ -0,0 +1,5 @@
+n1 = float(input("Digite um número qualquer: "))
+n2 = float(input("Digite outro número qualquer: "))
+n3 = float(input("Digite o último número qualquer: "))
+lista = [n1, n2, n3]
+print("A soma dos números digitados é {}".format(sum(lista)))
