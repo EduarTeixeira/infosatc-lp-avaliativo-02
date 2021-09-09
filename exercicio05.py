f2803f12c158ff05a9f09a3d41db3334f14138f9
EduarTeixeira <88851515+EduarTeixeira@users.noreply.github.com>
2021-09-09 15:53:44 -0300

    Create exercicio05.py

---
+++ exercicio05.py
@@ -0,0 +1,9 @@
+n1 = input("Digite o nome de um jogador de futebol: ")
+n2 = input("Digite o nome de mais um jogador de futebol: ")
+n3 = input("Digite o nome de outro jogador de futebol: ")
+lista = [n1, n2, n3]
+print(lista)
+if "Neymar" in lista:
+    print("Neymar está na lista")
+else:
+    print("Neymar não está na lista")
